34f3583a6679736a0dda3f2acd87f0d1beb88ec0
unknown <abhinav.behal@hotmail.com>
2017-10-09 17:18:28 +1300

    Fixed performance issues with Translator

--- src/tatai/model/Translator.java
+++ src/tatai/model/Translator.java
@@ -15,15 +15,15 @@ public class Translator {
     };
 
     public static String translate(int n) {
-        String output = "";
+        String output;
         if (n <= 10) {
             output = words[n - 1];
         } else if (n / 10 == 1) {
-            output = String.format("%s m\u0101 %s", words[9], words[n % 10 - 1]);
+            output = words[9] + " m\u0101 " + words[n % 10 - 1];
         } else if (n % 10 == 0) {
-            output = String.format("%s %s", words[n / 10 - 1], words[9]);
+            output = words[n / 10 - 1] + " " + words[9];
         } else {
-            output = String.format("%s %s m\u0101 %s", words[n / 10 - 1], words[9], words[n % 10 - 1]);
+            output = words[n / 10 - 1] + " " + words[9] + " m\u0101 " + words[n % 10 - 1];
         }
         return output;
     }
--- src/tatai/ui/Main.java
+++ src/tatai/ui/Main.java
@@ -1,8 +1,6 @@
 package tatai.ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
