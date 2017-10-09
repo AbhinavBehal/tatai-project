b01df4ca2cf24360ddf420f76a2d23e89f3f2055
Z-Qi <g.qiao97@gmail.com>
2017-10-09 18:03:16 +1300

    Added side bar for options WIP

--- src/tatai/ui/NavigationPage.java
+++ src/tatai/ui/NavigationPage.java
@@ -1,19 +1,25 @@
 package tatai.ui;
 
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.Stack;
 
 public class NavigationPage extends Scene {
 
+    @FXML
+    private StackPane parentPane;
     @FXML
     private StackPane content;
     @FXML
@@ -22,6 +28,8 @@ public class NavigationPage extends Scene {
     private MaterialDesignIconView optionsButton;
     @FXML
     private Label title;
+    @FXML
+    private GridPane optionsBar;
 
     private static final int BACKBUTTON_SIZE = 48;
     private Stack<Page> _pages;
@@ -52,8 +60,22 @@ public class NavigationPage extends Scene {
             popPage();
         });
         optionsButton.setOnMouseClicked(e -> {
-            if (!e.getButton().equals(MouseButton.PRIMARY)) return;
+            if (e.getButton().equals(MouseButton.PRIMARY)) {
+                System.out.println("pressed");
+                if (!optionsBar.isVisible() ||
+                        optionsBar.getBoundsInParent().getMinX() == parentPane.getWidth()) {
+                    System.out.println("popped");
+                    popup(true);
+                }
+            }
             _pages.peek().onOptionsButtonPressed();
+        });
+        parentPane.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
+            if (e.getPickResult().getIntersectedNode() == null) {
+                // do nothing
+            } else if (e.getPickResult().getIntersectedNode() != optionsBar && optionsBar.isVisible()) {
+                popup(false);
+            }
         });
     }
 
@@ -76,5 +98,26 @@ public class NavigationPage extends Scene {
     private void changePage() {
         content.getChildren().setAll(_pages.peek().getRoot());
         title.setText(_pages.peek().getTitle());
+    }
+
+    private void popup(boolean show) {
+        TranslateTransition tt = new TranslateTransition(Duration.millis(1000), optionsBar);
+        if (show) {
+            System.out.println("show: " + show);
+            optionsBar.setVisible(true);
+            tt.setFromX(parentPane.getWidth());
+            tt.setToX(parentPane.getWidth() - 150);
+        } else {
+            System.out.println("hide: " + show);
+            tt.setFromX(optionsBar.getBoundsInParent().getMinX());
+            tt.setToX(parentPane.getWidth());
+        }
+        tt.play();
+        tt.setOnFinished(e -> {
+            System.out.println("SHOW: " + show);
+            if (!show) {
+                optionsBar.setVisible(false);
+            }
+        });
     }
 }
